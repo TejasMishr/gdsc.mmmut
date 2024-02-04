9824975f76a4b156cfa542779fea5c0f5ce09d64
TejasMishr <tejasmishra72@gmail.com>
2024-02-05 01:01:21 +0530

    team img general structure done

---
+++ src/Team/Team_info/MemberInfo.jsx
@@ -0,0 +1,16 @@
+import Tejas from "../../assets/teamimg/Tejas.jpeg";
+
+const MembersData = [
+  {
+    img: Tejas,
+    memberName: "Tejas Mishra",
+    profession: "Jai Shree Ram",
+    FbLink: "",
+    InstaLink: "",
+    TwitterLink: "",
+    GithubLink: "https://github.com/TejasMishr",
+    LinkedinLink: "https://www.linkedin.com/in/tejasmishr/",
+  },
+];
+
+export default MembersData;
